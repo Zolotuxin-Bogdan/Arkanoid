904b76948c9bb474ac1b4f4334b593ec34ad0386
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-11-06 17:43:37 +0200

    Implemented Menu scene and scene managment

---
+++ Assets/Scripts/ControllScenesAndMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControllScenesAndMenu : MonoBehaviour
+{
+	public void NewGameLoadScene()
+    {
+        SceneManager.LoadScene("Game Scene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
