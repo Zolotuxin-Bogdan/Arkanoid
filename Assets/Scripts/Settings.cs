dee39e14579d7bb421818e7aa62b77afbcafd7df
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-09 19:35:39 +0200

    Added racket color choose in Settings menu

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject RacketSkin;
+
+    private Image _spriteImage;
+    private Color _newColor;
+
+    void Start()
+    {
+        _spriteImage = RacketSkin.GetComponent<Image>();
+        _newColor.r = 255;
+        _newColor.g = 255;
+        _newColor.b = 255;
+        _newColor.a = 255;
+    }
+
+    void Update()
+    {
+        _spriteImage.color = _newColor;
+    }
+
+    public void Red_Slider_Changed(float newValue)
+    {
+        _newColor = new Color(newValue, _spriteImage.color.g, _spriteImage.color.b);
+    }
+    public void Green_Slider_Changed(float newValue)
+    {
+        _newColor = new Color(_spriteImage.color.r, newValue, _spriteImage.color.b);
+    }
+    public void Blue_Slider_Changed(float newValue)
+    {
+        _newColor = new Color(_spriteImage.color.r, _spriteImage.color.g, newValue);
+    }
+}
