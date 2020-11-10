ad492c53c0f642ae43317ce024e755e93d5d24ef
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-10 12:25:10 +0200

    Renamed ControllScenesAndMenu -> ScenesController

---
+++ Assets/Scripts/ScenesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesController : MonoBehaviour
+{
+    ////////////////////////////////////////////////
+    //// Scene Loading
+    ////////////////////////////////////////////////
+    public void LoadNewGameScene()
+    {
+        SceneManager.LoadScene("Game Scene");
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    ////////////////////////////////////////////////
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    ////////////////////////////////////////////////
+    //// InGame Interactions
+    ////////////////////////////////////////////////
+    public void LoadNextLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void RetryLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    ////////////////////////////////////////////////
+
+}
