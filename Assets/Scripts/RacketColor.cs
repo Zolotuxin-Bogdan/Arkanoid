d32daee9b5e683d5e1849c883eabc9518a68ebc0
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-10 12:26:05 +0200

    Added RacketColor static script

---
+++ Assets/Scripts/RacketColor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketColor : MonoBehaviour
+{
+    public static float RedColor = 255f;
+    public static float GreenColor = 255f;
+    public static float BlueColor = 255f;
+}
