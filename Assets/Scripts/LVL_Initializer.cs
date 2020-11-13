e646950bab7f850783ed6eb6c8feb8dd2669aeb8
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-13 16:21:38 +0200

    Added lvl 2

--- Assets/Scripts/LVL_Initializer.cs
+++ Assets/Scripts/LVL_Initializer.cs
@@ -6,6 +6,7 @@ public class LVL_Initializer
 {
     private readonly List<LVL> _listOfLevels = new List<LVL>();
     private readonly LVL _lvl1 = new LVL();
+    private readonly LVL _lvl2 = new LVL();
 
     public LVL_Initializer()
     {
@@ -16,6 +17,17 @@ public class LVL_Initializer
         };
 
         _listOfLevels.Add(_lvl1);
+        ///////////////////////////////////////////////////////////////////////////////
+        
+        _lvl2.BlocksList = new List<(int, int)>
+        {
+            (-5, 3), (-3, 3), (-1, 3), (1, 3), (3, 3), (5, 3),
+            (-8, 2), (-6, 2), (-4, 2), (-2, 2), (0, 2), (2, 2), (4, 2), (6, 2), (8,2),
+            (-9, 1), (-5, 1), (-3, 1), (0, 1), (3, 1), (5, 1), (9, 1)
+        };
+
+        _listOfLevels.Add(_lvl2);
+
     }
     public LVL GetRandomLvl()
     {
