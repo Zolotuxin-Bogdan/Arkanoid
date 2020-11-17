4bbfeaa9ba6f6c3350840ef34f87a3b7dc5da285
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-17 18:14:13 +0200

    Implemented Block class

---
+++ Assets/Scripts/C# Classes/Block.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block
+{
+    public float XPosition { get; set; }
+    public float YPosition { get; set; }
+    public int BlockScoreCost { get; set; }
+
+    public Block(float xPosition, float yPosition, int blockScoreCost)
+    {
+        XPosition = xPosition;
+        YPosition = yPosition;
+        BlockScoreCost = blockScoreCost;
+    }
+}
