e925840d2ae4acd28bb82665b4d1d4a7ddddbb46
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-18 12:15:44 +0200

    Added implicit operator to custom Color type

--- Assets/Scripts/C# Classes/RacketColor.cs
+++ Assets/Scripts/C# Classes/RacketColor.cs
@@ -7,4 +7,9 @@ public class RacketColor
     public float RedColor { get; set; } = 255f;
     public float GreenColor { get; set; } = 255f;
     public float BlueColor { get; set; } = 255f;
+
+    public static implicit operator Color(RacketColor racketColor)
+    {
+        return new Color {r = racketColor.RedColor, g = racketColor.GreenColor, b = racketColor.BlueColor};
+    }
 }
