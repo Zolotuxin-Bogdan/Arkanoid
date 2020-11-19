eaa1e51f52211c69bb5d9550fb62084c367d4d9d
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-19 18:10:27 +0200

    Added Id field to Block class

--- Assets/Scripts/C# Classes/Block.cs
+++ Assets/Scripts/C# Classes/Block.cs
@@ -1,13 +1,18 @@
-﻿public class Block
+﻿using System.Threading;
+
+public class Block
 {
+    public int Id { get; set; }
     public float XPosition { get; set; }
     public float YPosition { get; set; }
     public int BlockScoreCost { get; set; }
 
-    public Block(float xPosition, float yPosition, int blockScoreCost)
+    public Block(int id, float xPosition, float yPosition, int blockScoreCost)
     {
+        Id = id;
         XPosition = xPosition;
         YPosition = yPosition;
         BlockScoreCost = blockScoreCost;
     }
+
 }
