d792eb84aacc161e7b4b915cd17442d719de6a05
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-20 14:36:16 +0200

    Implemented GameState class

---
+++ Assets/Scripts/C# Classes/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState
+{
+    public Level Level { get; set; }
+    public Level LevelState { get; set; }
+
+    public int GlobalScore { get; set; }
+    public int LocalScore { get; set; }
+
+    public Vector3 BallPosition { get; set; }
+    public Vector2 BallMovement { get; set; }
+
+    public Vector3 RacketPosition { get; set; }
+}
