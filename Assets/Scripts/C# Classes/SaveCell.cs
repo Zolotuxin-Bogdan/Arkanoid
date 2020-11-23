2ee934d85c8228bc49574f86b3b880214ac69b28
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-23 12:59:30 +0200

    Implemented SaveCell class

---
+++ Assets/Scripts/C# Classes/SaveCell.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCell
+{
+    public GameState GameState { get; set; }
+    public DateTime TimeStamp { get; set; }
+}
