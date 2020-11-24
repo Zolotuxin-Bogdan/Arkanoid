9f271d34b9aca3b5a6a2d827693282ba0c2333b8
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-24 20:26:23 +0200

    Implemented SessionStorage as Singleton

---
+++ Assets/Scripts/C# Classes/SessionStorage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionStorage
+{
+    private static SessionStorage _instance;
+    private static object syncRoot = new Object();
+
+    public bool GameLoaded { get; set; }
+    public int LoadCellIndex { get; set; }
+
+    public static SessionStorage Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (_instance == null)
+                        _instance = new SessionStorage();
+                }
+            }
+            return _instance;
+        }
+    }
+}
