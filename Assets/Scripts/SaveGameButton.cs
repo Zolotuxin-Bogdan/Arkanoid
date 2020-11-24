ed3072d448795cf186b4f3463654780b4b1fdefe
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-24 20:27:36 +0200

    Implemented SaveGameButton script

---
+++ Assets/Scripts/SaveGameButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SaveGameButton : MonoBehaviour
+{
+    public void SaveClick(int saveCellIndex)
+    {
+        GameController.Instance.SaveGame(saveCellIndex);
+    }
+
+    public void LoadClick(int loadCellIndex)
+    {
+        var _sessionStorage = SessionStorage.Instance;
+        _sessionStorage.LoadCellIndex = loadCellIndex;
+    }
+}
+
