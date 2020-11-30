58b888ca49fc53f70ccb045d4142085f623609fe
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-30 22:07:25 +0200

    Implemented DoubleBallBonusCommand

---
+++ Assets/Scripts/C# Classes/DoubleBallBonusCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class DoubleBallBonusCommand : IBonus
+{
+    public int BonusDropPercent;
+    private static Random _randomGen = new Random();
+
+    public DoubleBallBonusCommand(int bonusDropPercent)
+    {
+        BonusDropPercent = bonusDropPercent;
+    }
+    public void Execute()
+    {
+        var percentValue = _randomGen.Next(100);
+        if (percentValue < BonusDropPercent)
+        {
+            BallManager.Instance.SpawnDoubleBall();
+        }
+    }
+}
