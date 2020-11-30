814a98bed05d4e4cd73ae029ab1389f2a785a68e
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-30 22:03:08 +0200

    Implemented Ball class

---
+++ Assets/Scripts/C# Classes/Ball.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Ball
+{
+    public Vector3 BallPosition { get; set; }
+    public Vector2 BallMovement { get; set; }
+    public float DirectionX { get; set; }
+    public float DirectionY { get; set; }
+    public CustomColor BallColor { get; set; }
+
+}
