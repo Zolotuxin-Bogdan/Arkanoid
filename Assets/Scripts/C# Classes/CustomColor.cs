31691dea1da277e7f400f3ac21f04efcfa40cb37
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-11-30 22:01:43 +0200

    Renamed RacketColor -> CustomColor

---
+++ Assets/Scripts/C# Classes/CustomColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CustomColor
+{
+    public float RedColor { get; set; } = 255f;
+    public float GreenColor { get; set; } = 255f;
+    public float BlueColor { get; set; } = 255f;
+
+    public static implicit operator Color(CustomColor customColor)
+    {
+        return new Color {r = customColor.RedColor, g = customColor.GreenColor, b = customColor.BlueColor};
+    }
+
+    public static implicit operator CustomColor(Color color)
+    {
+        return new CustomColor { RedColor = color.r, GreenColor = color.g, BlueColor = color.b };
+    }
+}
